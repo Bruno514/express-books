98ba95a388f908329bd2a75e3404ce6259bf51fa
Bruno <86791361+Bruno514@users.noreply.github.com>
2025-03-13 22:23:43 -0300

    add method-override middleware and author router to app

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("node:path");
+const methodOverride = require("method-override");
 const indexRouter = require("./routes/indexRouter");
 const booksRouter = require("./routes/booksRouter");
 const genresRouter = require("./routes/genresRouter");
+const authorsRouter = require("./routes/authorsRouter");
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,9 +17,11 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(assetsPath));
+app.use(methodOverride('_method'));
 
 app.use("/", indexRouter);
 app.use("/books", booksRouter);
+app.use("/authors", authorsRouter);
 app.use("/genres", genresRouter);
 
 app.listen(PORT, () => {
