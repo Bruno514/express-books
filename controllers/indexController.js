2bf9a61167f650d8b0865376132e938e11ba3066
Bruno <86791361+Bruno514@users.noreply.github.com>
2025-03-14 08:48:37 -0300

    fix index controller render values

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,9 +2,13 @@ const db = require("../db/queries");
 
 exports.getIndex = async (req, res) => {
   const books = await db.getBooks();
+  const genres = await db.getGenres();
+  const authors = await db.getAuthors();
 
   res.render("index", {
     title: "Index Page",
     books: books,
+    genres: genres,
+    authors: authors,
   });
 };
