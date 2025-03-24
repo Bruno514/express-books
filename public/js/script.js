45c642456344832257cf76075c64ca27e6682e18
Bruno <86791361+Bruno514@users.noreply.github.com>
2025-03-24 14:28:26 -0300

    finish navbar and dropdown elements

---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+const dropdown1 = document.querySelectorAll(".dropdown")[0];
+const dropdown2 = document.querySelectorAll(".dropdown")[1];
+
+dropdown1.addEventListener("click", (e) => {
+  dropdown1.classList.toggle("visible");
+  dropdown2.classList.remove("visible");
+});
+dropdown2.addEventListener("click", (e) => {
+  dropdown2.classList.toggle("visible");
+  dropdown1.classList.remove("visible");
+});
+
+const hamburger = document.querySelector(".nav-hamburger");
+
+hamburger.addEventListener("click", (e) => {
+  const navMenu = document.querySelector(".nav-menu");
+  navMenu.classList.toggle("visible");
+});
